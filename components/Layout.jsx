b87694bca0c5d6438ccde346ebadecef3a342bd5
Jaydeep <jaydeep.dey03@gmail.com>
2022-05-23 15:58:35 +0530

    added navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
 
 const Layout = () => {
     return (
-        <div>
-            Layout
+        <div className='layout'>
+            <Head><title>Ecommerce-Sanity</title></Head>
+            <header><Navbar /></header>
         </div>
+
     )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {AiOutlineShopping} from 'react-icons/ai'
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='navbar-container'>
+            <p className='logo'>
+                <Link href={`/`}>JSM Headphones</Link>
+            </p>
+            <button type='button' className='cart-icon'>
+                <AiOutlineShopping />
+                <span className='cart-item-qty'>1</span>
+            </button>
         </div>
     )
 }
